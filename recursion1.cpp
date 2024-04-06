e24310cffd21a00b77436ceef14a1b16093e323d
naman-017 <92266293+naman-017@users.noreply.github.com>
2024-04-05 20:56:36 -0700

    Update recursion1.cpp

--- recursion1.cpp
+++ recursion1.cpp
@@ -111,3 +111,21 @@ bool check_palin_str(string s, int l, int n)
 		return false;
 	return check_palin_str(s, l+1, n);
 }
+
+int main()
+{
+	//fibonacci array
+	int n;
+	cin >> n;
+	int arr[n+1];
+	arr[0]=0;
+	arr[1]=1;
+	
+	for(int i=2; i<n+1; i++){
+		arr[i]=arr[i-1]+arr[i-2];
+	}
+
+	for(int i=0; i<n+1; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
